54ac87b2ca0ba1d823b0950bf12620f228c03be0
Pierce Morris <pierce.morris1998@gmail.com>
2022-07-15 21:18:55 +0100

    /

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,17 @@
 import type { HttpFunction } from '@google-cloud/functions-framework/build/src/functions';
+import { Pool } from 'pg';
 
-export const helloWorld: HttpFunction = (req, res) => {
-  res.send('Hello, World');
+const credentials = {
+  user: "piercemorris",
+  host: "localhost",
+  database: "competitive-standing",
+  port: 5432,
+};
+
+export const helloWorld: HttpFunction = async (req, res) => {
+  const pool = new Pool(credentials);
+  const players = await pool.query('SELECT * FROM players')
+  await pool.end()
+
+  res.send(players.rows);
 };
