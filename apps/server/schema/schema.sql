694c58f954db5f934923ed6ff123a446fc3c2db4
Pierce Morris <pierce.morris1998@gmail.com>
2022-07-19 21:15:08 +0100

    update schema

--- apps/server/schema/schema.sql
+++ apps/server/schema/schema.sql
@@ -1,20 +1,32 @@
 
-CREATE TABLE player (
+CREATE TABLE players (
 	id serial PRIMARY KEY,
-	name VARCHAR (50) NOT NULL,
+	player_name VARCHAR (50) NOT NULL,
 	created_on TIMESTAMP default current_timestamp
 );
 
-CREATE TABLE match (
+CREATE TABLE matches (
   id serial PRIMARY KEY,
-  game_type VARCHAR (50) NOT NULL,
+  game_type VARCHAR (50) NOT NULL, /* pool, table tennis ... */
+  match_type VARCHAR (50) NOT NULL, /* head-to-head ... */
+  score 
   date TIMESTAMP default current_timestamp
 );
 
-/* TODO: Figure out how to store match data and decide */
+CREATE TABLE player_matches (
+  match_id REFERENCES matches (id) ON UPDATE CASCADE ON DELETE CASCADE,
+  player_id REFERENCES players (id) ON UPDATE CASCADE,
+  score number NOT NULL default 0,
+  CONSTRAINT player_match_pkey PRIMARY KEY (match_id, player_id)
+);
 
-CREATE TABLE playerMatch (
-  match_id REFERENCES match (id) ON UPDATE CASCADE ON DELETE CASCADE,
-  player REFERENCES player (id) ON UPDATE CASCADE,
-  opponent REFERENCES player (id) ON UPDATE CASCADE
-)
+CREATE TABLE groups (
+  id serial PRIMARY KEY,
+  group_name VARCHAR (50) NOT NULL,
+);
+
+CREATE TABLE player_groups (
+  group_id REFERENCES groups (id) ON UPDATE CASCADE ON DELETE CASCADE,
+  player_id REFERENCES players (id) ON UPDATE CASCADE,
+  CONSTRAINT player_group_pkey PRIMARY KEY (group_id, player_id)
+);
